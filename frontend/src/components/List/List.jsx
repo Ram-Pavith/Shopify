68fdc5574eb585ed5b23b4e87eb49992acc72ce0
Ram-Pavith <krpavith22@gmail.com>
2023-05-03 12:12:12 +0530

    Fixes

--- frontend/src/components/List/List.jsx
+++ frontend/src/components/List/List.jsx
@@ -5,33 +5,27 @@ import Card from "../Card/Card";
 import useFetch from "../../hooks/useFetch";
 import axios from 'axios'
 import {BarLoader} from 'react-spinners'
-import { listProductsByCategory } from "../../actions/productActions";
 const List = ({ subCats, maxPrice, sort, category }) => {
-  //const [products,setProducts] = useState([])
-  //const [isLoading,setIsLoading] = useState(true)
-  // let api = `http://localhost:5000/api/products/category/${category}`
-  // if(category === 'All'){
-  //   api = 'http://localhost:5000/api/products'
-  // }
-  // console.log(category)
-  // useEffect(()=>{
-  //   const fetchProducts = async ()=>{
-  //     const data = await axios.get(api)//.then(data=>setProducts(data.data))
-  //     setProducts(data.data)
-  //     setIsLoading(false)
-  //     console.log(products)
-  //     console.log("from inside async func "+category)
-  //   }
-  //   fetchProducts()
-  // },[category])
-  //const productLists = useSelector((state)=>state.productList)
-  const {loading,error,data} = listProductsByCategory(category)
-  const products = data
-  console.log(products)
+  const [products,setProducts] = useState([])
+  const [isLoading,setIsLoading] = useState(true)
+  let api = `http://localhost:5000/api/products/category/${category}`
+  if(category === 'All'){
+    api = 'http://localhost:5000/api/products'
+  }
+  useEffect(()=>{
+    const fetchProducts = async ()=>{
+      const data = await axios.get(api)//.then(data=>setProducts(data.data))
+      setProducts(data.data)
+      setIsLoading(false)
+    }
+    fetchProducts()
+  },[category])
+
+
 
   return (
     <div className="list">
-      {loading
+      {isLoading
         ? <BarLoader/>
         : products?.map((item) => <Card item={item} key={item.product_id} />)}
     </div>
